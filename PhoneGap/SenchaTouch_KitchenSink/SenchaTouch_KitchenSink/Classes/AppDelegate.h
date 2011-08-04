f104c79fe349b3d647f714bf1e82d7acb3193902
Serene Khaos <serenekhaos@ymail.com>
2011-08-03 21:17:49 -0400

    Adding PhoneGap: Sencha Touch Kitchen Sink example app
    iOS app built with PhoneGap of the Sencha Touch Kitchen Sink demo

---
+++ PhoneGap/SenchaTouch_KitchenSink/SenchaTouch_KitchenSink/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  SenchaTouch_KitchenSink
+//
+//  Created by Kelvin Ling on 8/3/11.
+//  Copyright Serene Khaos 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit SenchaTouch_KitchenSink.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
